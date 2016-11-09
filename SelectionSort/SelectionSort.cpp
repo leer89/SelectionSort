3bcfc7acf9ee34b83ff3b40273aed68afb537cc8
leer89 <leer89@gmail.com>
2016-11-09 16:55:41 -0500

    - add sort and swap values - still writing index_of_smallest

--- SelectionSort/SelectionSort.cpp
+++ SelectionSort/SelectionSort.cpp
@@ -28,8 +28,8 @@ int main()
 	using namespace std;
 	cout << "This program sorts numbers from lowest to highest using Selection Sort.\n";
 
-	int sample_array[10], number_used;
-	fill_array(sample_array, 10, number_used);
+	int sample_array[100], number_used;
+	fill_array(sample_array, 100, number_used);
 	sort(sample_array, number_used);
 
 	cout << "In sorted order the numbers are:\n";
@@ -37,4 +37,47 @@ int main()
 	cout << sample_array[index] << " " << endl;
 
 	return 0;
+}
+
+// uses iostream:
+void fill_array(int a[], int size, int& number_used)
+{
+	using namespace std;
+	cout << "Enter up to " << size << "non-negative whole numbers.\n"
+		<< "Mark the end of the list with a negative number.\n";
+	int next, index = 0;
+	cin >> next;
+	while ((next >= 0) && (index < size))
+	{
+		a[index] = next;
+		index++;
+		cin >> next;
+	}
+	number_used = index;
+}
+
+void sort(int a[], int number_used)
+{
+	int index_of_next_smallest;
+	for (int index = 0; index < number_used - 1; index++)
+	{
+		// place the correct value in a[index]:
+		index_of_next_smallest = index_of_smallest(a, index, number_used);
+		swap_values(a[index], a[index_of_next_smallest]);
+		// a[0] <= a[1]...<= a[index] are the smallest of the original array
+		// elements. the rest of the elements are in the remaining positions
+	}
+}
+
+void swap_values(int& v1, int& v2)
+{
+	int temp;
+	temp = v1;
+	v1 = v2;
+	v2 = temp;
+}
+
+int index_of_smallest(const int a[], int start_index, int number_used)
+{
+	int min = a[start]
 }
